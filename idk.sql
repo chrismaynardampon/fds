df67fc004587312028ccf51b02135fa8dfe42a5f
Chris Maynard Ampon <110610747+Hearteu02@users.noreply.github.com>
2024-02-04 14:56:14 +0800

    Update idk.sql

--- idk.sql
+++ idk.sql
@@ -1,30 +1,22 @@
-CREATE PROCEDURE `sample2` (in studid int, in subid int, out output varchar(20))
+CREATE PROCEDURE `sample2` (in Studid int, in subid int, out output varchar(20))
 BEGIN
 declare num int;
 declare num2 int;
+declare num3 int;
+declare temp varchar(20);
+declare checker cursor for
+	select sched from (students left outer join enroll on students.studid = enroll.studid) left outer join subjects on subjects.subjid = enroll.subjid where enroll.studid = Studid;
+open checker;
+fetch checker into temp;
+
+
 set num = 829;
-call sample(studid, subid, @a,@b);
+call sample(Studid, subid, @a,@b);
 select @a into num2;
-if num < num2 then
+select @b into num3;
+if num < num2 and num > num3 then
 	set output = 'hatdog';
 else
-	set output = 'hatdkik';
+	set output = 'hatkik';
 end if;
-END
----------------------
-CREATE PROCEDURE `sample` (studid int, subid int, out output int, out output2 int)
-BEGIN
-	declare fullsched varchar(20);
-    declare sched_time varchar(10);
-    declare sched_start int;
-    declare sched_end int;
-	
-    select sched into fullsched from subjects where subjid = subid;
-    
-    set sched_time = right(fullsched, 7);
-    set sched_start = left(sched_time, 3);
-    set sched_end = right(sched_time, 3);
-    
-    set output = sched_start;
-    set output2 = sched_end;
 END
