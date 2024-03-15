8e26778de04a0283e1a06cbc122c5e3f08d310a0
Chris Maynard Ampon <110610747+Hearteu02@users.noreply.github.com>
2024-03-15 11:09:23 +0800

    Update exer3.py

--- exer3.py
+++ exer3.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-from tkinter import ttk
 from tkinter import messagebox
 import pymongo
 
@@ -18,36 +17,44 @@ label = tk.Label(window, text="Student Registration", width=20, height= 1, bg="y
 label.config(font=("Courier", 10))
 label.grid(column=2,row=1)
 
-label = tk.Label(window, text="Student ID:", width=15, height= 1, bg="yellow", anchor="center")
+label = tk.Label(window, text="Student ID:", width=20, height= 1, bg="yellow", anchor="center")
 label.grid(column=1,row=2)
 
-label = tk.Label(window, text="Student Name:", width=15, height= 1, bg="yellow", anchor="center")
+label = tk.Label(window, text="Student Name:", width=20, height= 1, bg="yellow", anchor="center")
 label.grid(column=1,row=3)
 
-label = tk.Label(window, text="Student Email:", width=15, height= 1, bg="yellow", anchor="center")
+label = tk.Label(window, text="Student Address:", width=20, height= 1, bg="yellow", anchor="center")
 label.grid(column=1,row=4)
 
-label = tk.Label(window, text="Student Course:", width=15, height= 1, bg="yellow", anchor="center")
+label = tk.Label(window, text="Student Contact:", width=20, height= 1, bg="yellow", anchor="center")
 label.grid(column=1,row=5)
 
+label = tk.Label(window, text="Student Course:", width=20, height= 1, bg="yellow", anchor="center")
+label.grid(column=1,row=6)
 
+label = tk.Label(window, text="Student Year Lvl:", width=20, height= 1, bg="yellow", anchor="center")
+label.grid(column=1,row=7)
 
 # table
 
-label = tk.Label(window, text="ID", width=13, height= 1, bg="yellow", anchor="center")
-label.grid(column=4,row=9)
-
-label = tk.Label(window, text="Name", width=13, height= 1, bg="yellow", anchor="center")
+label = tk.Label(window, text="Studid", width=13, height= 1, bg="yellow", anchor="center")
 label.grid(column=5,row=9)
 
-label = tk.Label(window, text="Email", width=13, height= 1, bg="yellow", anchor="center")
+label = tk.Label(window, text="Studname", width=13, height= 1, bg="yellow", anchor="center")
 label.grid(column=6,row=9)
 
-label = tk.Label(window, text="Course", width=13, height= 1, bg="yellow", anchor="center")
+label = tk.Label(window, text="Studadd", width=13, height= 1, bg="yellow", anchor="center")
 label.grid(column=7,row=9)
 
-label = tk.Label(window, text="TotalUnits", width=13, height= 1, bg="yellow", anchor="center")
+label = tk.Label(window, text="Studcontact", width=13, height= 1, bg="yellow", anchor="center")
 label.grid(column=8,row=9)
+
+label = tk.Label(window, text="Studcourse", width=13, height= 1, bg="yellow", anchor="center")
+label.grid(column=9,row=9)
+
+label = tk.Label(window, text="Studyear", width=13, height= 1, bg="yellow", anchor="center")
+label.grid(column=10,row=9)
+
 
 sid = tk.StringVar()
 studid = tk.Entry(window, textvariable=sid)
@@ -57,13 +64,21 @@ sname = tk.StringVar()
 studname = tk.Entry(window, textvariable=sname)
 studname.grid(column=2,row=3)
 
-semail = tk.StringVar()
-studemail = tk.Entry(window, textvariable=semail)
-studemail.grid(column=2,row=4)
+sadd = tk.StringVar()
+studadd = tk.Entry(window, textvariable=sadd)
+studadd.grid(column=2,row=4)
+
+scontact = tk.StringVar()
+studcontact = tk.Entry(window, textvariable=scontact)
+studcontact.grid(column=2,row=5)
 
 scourse = tk.StringVar()
 studcourse = tk.Entry(window, textvariable=scourse)
-studcourse.grid(column=2,row=5)
+studcourse.grid(column=2,row=6)
+
+studyr = tk.StringVar()
+studyear = tk.Entry(window, textvariable=studyr)
+studyear.grid(column=2,row=7)
 
 def msgbox(msg,titlebar):
     result=messagebox.askokcancel(title=titlebar, message=msg)
@@ -74,8 +89,10 @@ def callback(event):
     li=event.widget._values
     sid.set(studrec[li[1]][0])
     sname.set(studrec[li[1]][1])
-    semail.set(studrec[li[1]][2])
-    scourse.set(studrec[li[1]][3])
+    sadd.set(studrec[li[1]][2])
+    scontact.set(studrec[li[1]][3])
+    scourse.set(studrec[li[1]][4])
+    studyr.set(studrec[li[1]][5])
     
 def deletegrid():
     for label in window.grid_slaves():
@@ -84,18 +101,18 @@ def deletegrid():
 def creategrid():
     global studrec
     students = list(mycol.find({}))
-    studrec = [[stud['studid'], stud['studname'], stud['studemail'], stud['studcourse'], 0] for stud in students]
+    studrec = [[stud['studid'], stud['studname'], stud['studadd'],stud['studcontact'], stud['studcourse'], stud['studyear']] for stud in students]
     for i in range(len(studrec)):
         for j in range(len(studrec[0])):
             mgrid = tk.Entry(window,width=15)
             mgrid.insert(tk.END, studrec[i][j])
             mgrid._values = mgrid.get(), i
-            mgrid.grid(row=i+10, column=j+4)
+            mgrid.grid(row=i+10, column=j+5)
             mgrid.bind("<Button-1>", callback)
 def save():
     r=msgbox("save record","record")
     if r==True:
-        mycol.insert_one({"studid": int(studid.get()),"studname": studname.get(), "studemail": studemail.get(), "studcourse": studcourse.get()})
+        mycol.insert_one({"studid": int(studid.get()),"studname": studname.get(), "studadd": studadd.get(), "studcontact": studcontact.get(), "studcourse": studcourse.get(), "studyear": studyear.get()})
         deletegrid()
         creategrid()
 
@@ -103,8 +120,10 @@ def update():
     r=msgbox("update record","record")
     if r==True:
         mycol.update_one({"studid": int(studid.get())}, {"$set":{"studname": studname.get()}})
-        mycol.update_one({"studid": int(studid.get())}, {"$set":{"studemail": studadd.get()}})
+        mycol.update_one({"studid": int(studid.get())}, {"$set":{"studadd": studadd.get()}})
+        mycol.update_one({"studid": int(studid.get())}, {"$set":{"studcontact": studcontact.get()}})
         mycol.update_one({"studid": int(studid.get())}, {"$set":{"studcourse": studcourse.get()}})
+        mycol.update_one({"studid": int(studid.get())}, {"$set":{"studyear": studyear.get()}})
         deletegrid()
         creategrid()
 def delete():
@@ -115,63 +134,14 @@ def delete():
         creategrid()
 
 savebtn = tk.Button(text="Save", command=save)
-savebtn.grid(column=1,row=6)
+savebtn.grid(column=1,row=8)
 
 deletebtn = tk.Button(text = "Delete", command=delete)
-deletebtn.grid(column=2,row=6)
+deletebtn.grid(column=2,row=8)
 
 updatebtn = tk.Button(text = "Update", command=update)
-updatebtn.grid(column=3,row=6)
+updatebtn.grid(column=3,row=8)
 
-#exer4
-
-#row3
-label = tk.Label(window, text="Filter:", width=10, height= 1, bg="yellow", anchor="center")
-label.grid(column=4,row=3)
-
-label = tk.Label(window, text="Name Start:", width=10, height= 1, bg="yellow", anchor="center")
-label.grid(column=5,row=3)
-
-#row4
-label = tk.Label(window, text="ID:", width=10, height= 1, bg="yellow", anchor="center")
-label.grid(column=4,row=4)
-
-filter_nameStart = tk.StringVar()
-filter_nameStartEntry = tk.Entry(window, textvariable=filter_nameStart, width=12)
-filter_nameStartEntry.grid(column=5,row=4)
-
-#row5
-current_var = tk.StringVar()
-combobox_filter = ttk.Combobox(window, textvariable=current_var, width=9)
-combobox_filter.grid(column=4, row=5)
-combobox_filter['values'] = ('>', '<', '>=', '<=', '=', '!=')
-current_value = current_var.get() #get selected value
-
-label = tk.Label(window, text="Name End:", width=10, height= 1, bg="yellow", anchor="center")
-label.grid(column=5,row=5)
-
-label = tk.Label(window, text="Mail Start:", width=10, height= 1, bg="yellow", anchor="center")
-label.grid(column=6,row=5)
-
-label = tk.Label(window, text="Course Start:", width=10, height= 1, bg="yellow", anchor="center")
-label.grid(column=7,row=5)
-
-#row6
-filter_idNum = tk.StringVar()
-filter_idNumEntry = tk.Entry(window, textvariable=filter_idNum, width=12)
-filter_idNumEntry.grid(column=4,row=6)
-
-filter_nameEnd = tk.StringVar()
-filter_nameEndEntry = tk.Entry(window, textvariable=filter_nameEnd, width=12)
-filter_nameEndEntry.grid(column=5,row=6)
-
-filter_mail = tk.StringVar()
-filter_mailEntry = tk.Entry(window, textvariable=filter_mail, width=12)
-filter_mailEntry.grid(column=6,row=6)
-
-filter_course = tk.StringVar()
-filter_courseEntry = tk.Entry(window, textvariable=filter_course, width=12)
-filter_courseEntry.grid(column=7,row=6)
 
 creategrid()
 window.mainloop()
