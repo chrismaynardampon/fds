87ad6642c97d599b71d467108464d4c770f6e571
Chris Maynard Ampon <110610747+Hearteu02@users.noreply.github.com>
2024-03-16 00:07:51 +0800

    Add files via upload

--- exer4.py
+++ exer4.py
@@ -14,6 +14,7 @@ window.configure(bg="orange")
 
 studrec = [[]]
 result = mycol.find({})
+current_value = ''
 
 label = tk.Label(window, text="Student Registration", width=20, height= 1, bg="yellow", anchor="center")
 label.config(font=("Courier", 10))
@@ -82,9 +83,37 @@ def deletegrid():
     for label in window.grid_slaves():
         if (int(label.grid_info()["row"]) > 7):
             label.grid_forget()
-def creategrid(result):
-    global studrec
-    students = list(result)
+def creategrid():
+    deletegrid()
+    global studrec, current_value
+    if current_value == '>':
+        student = mycol.find({"studid": {"$gt": int(filter_idNumEntry.get())},
+        "studname": {"$regex": "^"+filter_nameStartEntry.get()+".*"+filter_nameEndEntry.get()+"$"},
+        "studemail":{"$regex": "^"+filter_mailEntry.get()}, "studcourse":{"$regex": "^"+filter_courseEntry.get()}})
+    elif current_value == '<':
+        student = mycol.find({"studid":{"$lt":int(filter_idNumEntry.get())},
+        "studname": {"$regex": "^"+filter_nameStartEntry.get()+".*"+filter_nameEndEntry.get()+"$"},
+        "studemail":{"$regex": "^"+filter_mailEntry.get()}, "studcourse":{"$regex": "^"+filter_courseEntry.get()}})
+    elif current_value == '>=':
+        student = mycol.find({"studid":{"$gte":int(filter_idNumEntry.get())},
+        "studname": {"$regex": "^"+filter_nameStartEntry.get()+".*"+filter_nameEndEntry.get()+"$"},
+        "studemail":{"$regex": "^"+filter_mailEntry.get()}, "studcourse":{"$regex": "^"+filter_courseEntry.get()}})
+    elif current_value == '<=':
+        student = mycol.find({"studid":{"$lte":int(filter_idNumEntry.get())},
+        "studname": {"$regex": "^"+filter_nameStartEntry.get()+".*"+filter_nameEndEntry.get()+"$"},
+        "studemail":{"$regex": "^"+filter_mailEntry.get()}, "studcourse":{"$regex": "^"+filter_courseEntry.get()}})
+    elif current_value == '!=':
+        student = mycol.find({"studid":{"$ne":int(filter_idNumEntry.get())},
+        "studname": {"$regex": "^"+filter_nameStartEntry.get()+".*"+filter_nameEndEntry.get()+"$"},
+        "studemail":{"$regex": "^"+filter_mailEntry.get()}, "studcourse":{"$regex": "^"+filter_courseEntry.get()}})
+    elif current_value == '=':
+        student = mycol.find({"studid":{"$eq":int(filter_idNumEntry.get())},
+        "studname": {"$regex": "^"+filter_nameStartEntry.get()+".*"+filter_nameEndEntry.get()+"$"},
+        "studemail":{"$regex": "^"+filter_mailEntry.get()}, "studcourse":{"$regex": "^"+filter_courseEntry.get()}})
+    else:
+        student = mycol.find({"studname": {"$regex": "^"+filter_nameStartEntry.get()+".*"+filter_nameEndEntry.get()+"$"},
+        "studemail":{"$regex": "^"+filter_mailEntry.get()}, "studcourse":{"$regex": "^"+filter_courseEntry.get()}})
+    students = list(student)
     studrec = [[stud['studid'], stud['studname'], stud['studemail'], stud['studcourse'], 0] for stud in students]
     for i in range(len(studrec)):
         for j in range(len(studrec[0])):
@@ -97,7 +126,6 @@ def save():
     r=msgbox("save record","record")
     if r==True:
         mycol.insert_one({"studid": int(studid.get()),"studname": studname.get(), "studemail": studemail.get(), "studcourse": studcourse.get()})
-        deletegrid()
         creategrid()
 
 def update():
@@ -106,19 +134,13 @@ def update():
         mycol.update_one({"studid": int(studid.get())}, {"$set":{"studname": studname.get()}})
         mycol.update_one({"studid": int(studid.get())}, {"$set":{"studemail": studadd.get()}})
         mycol.update_one({"studid": int(studid.get())}, {"$set":{"studcourse": studcourse.get()}})
-        deletegrid()
         creategrid()
 
 def delete():
     r=msgbox("delete record","record")
     if r==True:
         mycol.delete_one({"studid": int(studid.get())})
-        deletegrid()
         creategrid()
-
-def filter_table(result):
-    deletegrid()
-    creategrid(result)
 
 savebtn = tk.Button(text="Save", command=save)
 savebtn.grid(column=1,row=6)
@@ -148,10 +170,11 @@ filter_nameStartEntry.grid(column=5,row=4)
 
 #row5
 current_var = tk.StringVar()
-combobox_filter = ttk.Combobox(window, textvariable=current_var, width=9)
-combobox_filter.grid(column=4, row=5)
-combobox_filter['values'] = ('>', '<', '>=', '<=', '=', '!=')
-current_value = current_var.get() #get selected value
+def selection(e):
+    global current_value
+    current_value = e
+dropdown = tk.OptionMenu(window, current_var, '>', '<', '>=', '<=', '=', '!=', command=selection)
+dropdown.grid(column=4, row=5)
 
 label = tk.Label(window, text="Name End:", width=10, height= 1, bg="yellow", anchor="center")
 label.grid(column=5,row=5)
@@ -179,15 +202,8 @@ filter_course = tk.StringVar()
 filter_courseEntry = tk.Entry(window, textvariable=filter_course, width=12)
 filter_courseEntry.grid(column=7,row=6)
 
-#query
-mongoquery = mydb.get_collection("students")
-if current_value == ">":
-    result = mongoquery.find({"studid":{"$gt":filter_idNumEntry.get()}})
-else:
-    result = mongoquery.find({})
-
-filterbtn = tk.Button(text = "Filter", command=filter_table(mongoquery.find({"studid":{"$gt":filter_idNumEntry.get()}})))
+filterbtn = tk.Button(text = "Filter", command=creategrid)
 filterbtn.grid(column=9,row=6)
 
-creategrid(result)
+creategrid()
 window.mainloop()
